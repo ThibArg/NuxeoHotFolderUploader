c180959c243afbb4d138517d6cf409ec6cc066a3
Thibaud Arguillere <thibarg.web@free.fr>
2014-03-26 11:02:31 -0400

    Add a log file. Update README

--- NuxeoHotFolderUploader.sh
+++ NuxeoHotFolderUploader.sh
@@ -1,53 +1,77 @@
 #!/bin/bash
+# ============================================================
+# Change the following variables to match your environment
+# ============================================================
 SERVER_BASE_URL=http://localhost:8080/nuxeo
 USER_LOGIN=Administrator
 USER_PWD=Administrator
 # This folder, or workspace, or container *must*:
-#	- Exists
-#	- Have ACL which allows USER_LOGIN to create document
+# - Exists
+# - Have ACL which allows USER_LOGIN to create document
 NUXEO_DESTINATION_URL=$SERVER_BASE_URL/api/v1/path/default-domain/workspaces/hot-folder-import
 
+# This is the lazzy way of logging... It will work
+# on Linux, Mac, with no access right problem.
+# But maybe not the best way not the best place
+# to store a log ;->
+LOG_PATH=$HOME/nuxeo-hot-folder-uploader.log
 
+
+# ============================================================
+# Other variables
+# ============================================================
+# COPY_DEST_FOLDER will ve possibly filled if the optionnal "<optionnal copy folder>" argument is passed
+# (see print_usage)
 COPY_DEST_FOLDER=""
 
+
+# ============================================================
+# Main functions
+# ============================================================
 function watch_folder {
+  has_files=0
 
 # Don't spend time on empty folders
 if [ "$(ls -A $1)" ]; then
 
   cd "$1"
   for file in "$1"/*; do
+# Ignore invisible files, or system which adds /* for empty folders
+    if [[ $file != .* && $file != "*" && $file != "$1/*" ]]; then
 
-# Ignore invisible file, or system which adds /* for empty folders
-  if [[ $file != .* && $file != "*" && $file != "$1/*" ]]; then
+      if [ "$has_files" = "0" ]; then
+        { echo "========== <new_import>"; date; } | tee -a $LOG_PATH
+        has_files=1
+      fi
 
-    nuxeo_doc_type="File"
-    mime_type=`file --mime-type -b "$file"`
-    case "$mime_type" in
-      image/*) 
-        nuxeo_doc_type="Picture";;
-      
-      video/*)
-        nuxeo_doc_type="Video";;
+      nuxeo_doc_type="File"
+      mime_type=`file --mime-type -b "$file"`
+      case "$mime_type" in
+        image/*) 
+          nuxeo_doc_type="Picture";;
         
-      *)
-          case $file in
-          *.mov)
-            nuxeo_doc_type="Video";;
-          *.ORF)
-            nuxeo_doc_type="Picture";;
-          *.xmp)
-            nuxeo_doc_type="Picture";;
-          *)
-            nuxeo_doc_type="File";;
-        esac
-    esac
-  
-    send_to_nuxeo "$file" "$nuxeo_doc_type"
-  
-  fi
-
+        video/*)
+          nuxeo_doc_type="Video";;
+          
+        *)
+            case $file in
+            *.mov)
+              nuxeo_doc_type="Video";;
+            *.ORF)
+              nuxeo_doc_type="Picture";;
+            *.xmp)
+              nuxeo_doc_type="Picture";;
+            *)
+              nuxeo_doc_type="File";;
+          esac
+      esac
+      send_to_nuxeo "$file" "$nuxeo_doc_type"
+    fi
   done
+
+  if [ "$has_files" = "1" ]; then
+    { echo ""; date; echo "========== </new_import>"; } | tee -a $LOG_PATH
+  fi
 
 fi
 }
@@ -58,16 +82,18 @@ function send_to_nuxeo {
     filename="${file_full_path##*/}"
     filename_clean=${filename// /_}
   
-    echo "Sending file $filename, type $doc_type"
-    curl -H "X-Batch-Id: $filename_clean" -H "X-File-Idx:0" -H "X-File-Name:$filename" -F file=@"$file_full_path" -u "${USER_LOGIN}":"${USER_PWD}" "${SERVER_BASE_URL}/api/v1/automation/batch/upload"
+    echo "Sending file $filename, type $doc_type" >> $LOG_PATH
+    curl -H "X-Batch-Id: $filename_clean" -H "X-File-Idx:0" -H "X-File-Name:$filename" -F file=@"$file_full_path" -u "${USER_LOGIN}":"${USER_PWD}" "${SERVER_BASE_URL}/api/v1/automation/batch/upload" | tee -a $LOG_PATH
     
-    #echo ""
-    #echo "CHECKING THE BATCH"
-    #curl -u "${USER_LOGIN}":"${USER_PWD}" "${SERVER_BASE_URL}/api/v1/automation/batch/files/$filename_clean"
+    # Used during devug, mainly
+    #echo "" >> $LOG_PATH
+    #echo "CHECKING THE BATCH" >> $LOG_PATH
+    #curl -u "${USER_LOGIN}":"${USER_PWD}" "${SERVER_BASE_URL}/api/v1/automation/batch/files/$filename_clean" >> $LOG_PATH | tee -a $LOG_PATH
   
-    echo "Creating a $doc_type document"
-    curl -X POST -H "Content-Type: application/json" -u "${USER_LOGIN}":"${USER_PWD}" -d "{ \"entity-type\": \"document\", \"name\":\"${filename_clean}\", \"type\": \"${doc_type}\", \"properties\" : { \"dc:title\":\"${filename}\",\"file:content\": {\"upload-batch\":\"${filename_clean}\",\"upload-fileId\":\"0\"}}}" "${NUXEO_DESTINATION_URL}"
+    { echo ""; echo "Creating a $doc_type document"; } >> $LOG_PATH
+    curl -X POST -H "Content-Type: application/json" -u "${USER_LOGIN}":"${USER_PWD}" -d "{ \"entity-type\": \"document\", \"name\":\"${filename_clean}\", \"type\": \"${doc_type}\", \"properties\" : { \"dc:title\":\"${filename}\",\"file:content\": {\"upload-batch\":\"${filename_clean}\",\"upload-fileId\":\"0\"}}}" "${NUXEO_DESTINATION_URL}" | tee -a $LOG_PATH
   
+    { echo ""; echo "";}  >> $LOG_PATH
     if [ -n "$COPY_DEST_FOLDER" ]; then
       mv "$filename" "$COPY_DEST_FOLDER/$filename"
     else
@@ -76,9 +102,9 @@ function send_to_nuxeo {
 }
 
 function print_usage {
-  echo "Usage: upload_files.sh <folder> <optionnal copy folder>"
+  echo "Usage: upload_files.sh <path-to-hotfolder> <dest-container-in-nuxeo> <optionnal copy folder>"
   echo "    If <optionnal copy folder> is used, files are moved to"
-  echo "    this folder instead of being deleted"
+  echo "    this folder instead of being deleted afetr being sent."
   echo "    (Mainly used during development/debug.)"
 }
 
@@ -90,7 +116,9 @@ then
   fi
   watch_folder "$1"
 else
-  echo "Error: folder doesn't exist"
+  echo "Error: hot folder doesn't exist"
   print_usage
   exit 1
 fi
+
+#--EOF--
--- README.md
+++ README.md
@@ -6,6 +6,7 @@ The script get all the files in a folder and create documents (`File`, `Picture`
 It runs on Linux and Mac OS.
 
 ###Usage
+_Do not forget to make the .sh executable (for example, chmod +x /the/script)_ 
 ```
 /path/to/the/script.sh "/path/to/the/hotfolder"
 ```
@@ -18,13 +19,15 @@ Parameters:
 * $1 is the path to the ot folder
 * $2 is optionnal: Path to a backup folder
 
-If `$2` is used, files sent to Nuxeo are moved (`mv`) to this folder instead of being deleted (`rm`).
+Once a file has been sent:
+
+* If `$2`was passed, the file is moved to this folder
+* Else, the file is _permanently_ deleted (`rm`)
+  * __WARNING__: The script does not check errors returned by `curl`. So, whatever happens, the file will be deleted (see "Room for enhancement").
 
 
 ###Main principles
 The script gets the kind (Nuxeo document type:  `File`, `Picture` or `Video`) of the binary file, based on its mime-type. It adds some adjustement when our testing showed that a `File`document was created for a `raw`picture for example.
-
-The script ignores files whose name starts with "." (invisible.)
 
 What you may need to change:
 
@@ -34,6 +37,7 @@ What you may need to change:
   * `NUXEO_DESTINATION_URL`:
     * The destination path, on nuxeo server.
     * *IMPORTANT*: Make sure the user (`USER_LOGIN`/`USER_PWD`) has the right to create document at this path
+  * `LOG_PATH`: This one is set to save the log in the $HOME folder of the current user. Something you may want to change.
 * Mime-type detection: Add your specific types, Nuxeo document types, etc.
 
 Also, make sure the Linux/Mac OS user running the script has enough right to read/write the hot folder.
@@ -43,8 +47,9 @@ Once you are happy with the script, just add it to your `crontab` (or similar)
 ###Room for enhancement
 The script is quite simple. It was built for a quick Proof of Concept, and deserves to be enhanced. For example:
 
-* Better error handling: Maybe if an error occurs, everything must be stopped?
-* Better log: Everything is done _via_ `echo`. It would be better to have a log file, filled by the script. Every time it is launched, it would add someting to the log, etc.
+* Better error handling:
+  * If an error occurs with `curl`, do not delete the file, move it somewhere else (when `$2`is not passed)
+  * If an error occurs, report it in the log
 * . . .
 
 
